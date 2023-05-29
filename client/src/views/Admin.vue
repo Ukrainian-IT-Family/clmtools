a7135dfa92db33dbe98d1b00e2f7bbcf7bbf008b
DmytroYahuza <dmitry.yaguza@ukrainian-it.family>
2023-05-29 23:31:12 +0300

    Admin user filter fix

--- client/src/views/Admin.vue
+++ client/src/views/Admin.vue
@@ -230,7 +230,9 @@ export default {
 
     filterUsers() {
       this.allUsers = !this.filterRole ? this.GET_ALL_USERS : this.GET_ALL_USERS.filter(item => {
-        return item.role === this.filterRole;
+          console.log(this.filterRole);
+          // return (item.role === null) ? (this.filterRole === 'inactive') : item.role === this.filterRole;
+          return item.role === this.filterRole || (item.role === null && this.filterRole === "null");
       });
     },
 
